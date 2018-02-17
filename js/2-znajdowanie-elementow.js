076b3a24c7d083ef66b0698d41601039880cbb31
kajetans <66szukaj@gmail.com>
2018-02-17 12:01:21 +0100

    wskazanie elementu superlink

--- js/2-znajdowanie-elementow.js
+++ js/2-znajdowanie-elementow.js
@@ -1,11 +1,23 @@
 "use strict";
 
-var parFirst = documentElementById('parFirst');
-console.log(parFirst);
+// var parFirst = documentElementById('parFirst');
+// console.log(parFirst);
 // console.log(window.innerHeight);
 // console.log(window.outerHeight);
 
-console.log(Document);
+// var linkiPoTagu = document.getElementsByTagName('a');
+// console.log(linkiPoTagu);
+
+// var divPoId = document.querySelector('#parSecond');
+// console.log(divPoId);
+//
+// var pierwszyLinkPoSelektorze =
+// document.querySelector('.superlink')
+// console.log(pierwszyLinkPoSelektorze);
+
+var linkiPoSelektorze =
+document.querySelectorAll('.superlink');
+console.log(linkiPoSelektorze);
 // console.log(document.documentElement);
 // console.log(document.head);
 // console.log(document.body);
