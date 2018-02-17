075dc2c2bdd5dbe3db1ab4ea2121be53f9b232b0
kajetans <66szukaj@gmail.com>
2018-02-17 11:45:44 +0100

    dodanie css

---
+++ js/2-znajdowanie-elementow.js
@@ -0,0 +1,11 @@
+"use strict";
+
+var parFirst = documentElementById('parFirst');
+console.log(parFirst);
+// console.log(window.innerHeight);
+// console.log(window.outerHeight);
+
+console.log(Document);
+// console.log(document.documentElement);
+// console.log(document.head);
+// console.log(document.body);
